8d2a665613128a1cc94b3566a066266d3e803f07
DashaGaleeva <125319354+DashaGaleeva@users.noreply.github.com>
2025-09-15 18:25:02 +0300

    Update создание списка и стека на C++.cpp

--- Лабораторная работа № 1. Создание списка и стека/создание списка и стека на C++.cpp
+++ Лабораторная работа № 1. Создание списка и стека/создание списка и стека на C++.cpp
@@ -1,3 +1,16 @@
-#создание переменной типа список на C++
+//создание переменной типа список на C++
+#include <iostream>
+#include <list>
+int main() {
+    std::list<int> List = {1, 2, 3}
+}
 
-#создание пременной типа стек на C++
+//создание пременной типа стек на C++
+#include <iostream>
+#include <stack>
+int main() {
+    std::stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3)
+}
