cb136cc795a8654182a162b9f4a8c9e224cc5a78
DashaGaleeva <125319354+DashaGaleeva@users.noreply.github.com>
2025-09-15 18:16:45 +0300

    Update создание списка и стека на Java.java

--- Лабораторная работа № 1. Создание списка и стека/создание списка и стека на Java.java
+++ Лабораторная работа № 1. Создание списка и стека/создание списка и стека на Java.java
@@ -1,3 +1,19 @@
 #создание переменной типа список на Java
-
+public class Main {
+    public static void main(String[] args) {
+        String[] list = new String [3];
+        list[0] = "1";
+        list[1] = "2";
+        list[2] = "3"
+    }
+}
 #создание пременной типа стек на Java
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+    }
+}
